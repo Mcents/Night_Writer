361c48688f6d691ae04b06766b25bec3f198480f
Michael Centrelli <centm860@newschool.edu>
2017-05-13 14:06:07 -0600

    Third commit

--- lib/english_to_braille.rb
+++ lib/english_to_braille.rb
@@ -1,2 +1,20 @@
-require relative 'alphabet'
+require './lib/alphabet'
 require 'pry'
+class EnglishToBraille
+  attr_accessor :letters,
+                :translate
+  def initialize
+    @letters = Alphabet.new
+  end
+
+  def translate(input)
+    split_word = input.join.chars
+    translate_each_letter = split_word.map do |letter|
+      letters.hash[letter]
+    end
+    t = translate_each_letter.transpose
+    "#{t[0].join}\n" + "#{t[1].join}\n" + "#{t[2].join}"
+  end
+
+end
+puts @letters
